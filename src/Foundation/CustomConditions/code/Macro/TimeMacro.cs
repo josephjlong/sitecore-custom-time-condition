b0e071a5bc5bc93415731bddc000a332f755c06a
Joseph Long <joseph.long@sitecore.com>
2022-08-16 09:47:25 +0100

    Refactored to Helix principles.

---
+++ src/Foundation/CustomConditions/code/Macro/TimeMacro.cs
@@ -0,0 +1,26 @@
+﻿using Sitecore;
+using Sitecore.Diagnostics;
+using Sitecore.Rules.RuleMacros;
+using Sitecore.Text;
+using Sitecore.Web.UI.Sheer;
+using System.Xml.Linq;
+
+namespace SitecoreDemo.Foundation.CustomCondtions.Macro
+{
+    public class TimeMacro : IRuleMacro
+    {
+        public void Execute(XElement element, string name, UrlString parameters, string value)
+        {
+
+
+            Assert.ArgumentNotNull((object)element, "element");
+            Assert.ArgumentNotNull((object)name, "name");
+            Assert.ArgumentNotNull((object)parameters, "parameters");
+            Assert.ArgumentNotNull((object)value, "value");
+
+            SheerResponse.ShowModalDialog(new UrlString(UIUtil.GetUri("control:Sitecore.Shell.Applications.Dialogs.TimeSelector")).ToString(), "580px", "475px", string.Empty, true);
+
+        }
+
+    }
+}
